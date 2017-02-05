614f1f975f80874a187a048ea1c4621af81e5c73
Dean Blackketter <dean@blackketter.com>
2017-02-05 10:46:11 -0800

    Fix issue with incompatible String class libraries

--- src/PString.h
+++ src/PString.h
@@ -8,7 +8,13 @@ class PString : public String, public Print {
   public:
     virtual size_t write(uint8_t character) { concat(character); return 1; }
     virtual size_t write(const char *str) { concat(str); return strlen(str); }
+
+// two built-in libraries have slightly different APIs for appending a string of a specified length
+#ifdef ESP8266
     virtual size_t write(const uint8_t *buffer, size_t size) { concat((const char*)buffer, size); return size; }
+#else
+    virtual size_t write(const uint8_t *buffer, size_t size) { append((const char*)buffer, size); return size; }
+#endif
 };
 
 #endif
