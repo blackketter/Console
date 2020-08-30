5c5a5056df051b1f5b6485c34d9afc13c8c69da6
Dean Blackketter <dean@blackketter.com>
2020-08-30 10:20:05 -0700

    Add Teensy LC/3.x/4.x reboot and reload commands

---
+++ src/Commands/TeensyCommands.h
@@ -0,0 +1,46 @@
+#include "Command.h"
+#include "core_pins.h"
+#define CPU_RESTART_ADDR (uint32_t *)0xE000ED0C
+#define CPU_RESTART_VAL 0x5FA0004
+#define CPU_RESTART (*CPU_RESTART_ADDR = CPU_RESTART_VAL);
+
+// System reboot
+void reboot() {
+      while (1) {
+        delay(100);
+        CPU_RESTART
+      };
+}
+
+// Firmware upload
+void reload() {
+#if defined(__IMXRT1062__) || defined(__MK66FX1M0__) || defined(__MKL26Z64__)
+      asm("bkpt #251"); // run bootloader
+#endif
+}
+
+class RebootCommand : public Command {
+  public:
+    const char* getName() { return "reboot"; }
+    const char* getHelp() { return "Reboots system"; }
+    void execute(Console* c, uint8_t paramCount, char** params) {
+      c->println("Rebooting...");
+      delay(10);
+      reboot();
+    }
+};
+
+class ReloadCommand : public Command {
+  public:
+    const char* getName() { return "reload"; }
+    const char* getHelp() { return "Jump to Teensy bootloader"; }
+    void execute(Console* c, uint8_t paramCount, char** params) {
+      c->println("Jumping to bootloader...");
+      delay(10);
+      reload();
+    }
+};
+
+// add these to your sketch as needed
+RebootCommand theRebootCommand;
+ReloadCommand theReloadCommand;
