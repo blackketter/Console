6d99e043ea3f839600162f92da47e3c78b3a53af
Dean Blackketter <dean@blackketter.com>
2021-03-03 11:01:55 -0800

    Add parameters for non-default i2c pins to i2c command

--- src/Commands/i2cCommand.h
+++ src/Commands/i2cCommand.h
@@ -7,12 +7,32 @@
 class i2cCommand : public Command {
   public:
     const char* getName() { return "i2c"; }
-    const char* getHelp() { return ("i2c scan"); }
+    const char* getHelp() { return ("<sda scl frequency> - i2c bus scan"); }
     void execute(Console* c, uint8_t paramCount, char** params) {
       uint8_t error, address, nDevices;
+      int sda = SDA;
+      int scl = SCL;
+      uint32_t freq = 0;
 
-      c->println("i2c scan:");
+      if (paramCount > 0) {
+        sda = atoi(params[1]);
+      }
+      if (paramCount > 1) {
+        scl = atoi(params[2]);
+      }
+      if (paramCount > 2) {
+        freq = atoi(params[3]);
+      }
+
+      c->printf("i2c scan with SDA pin: %d, SCL pin: %d, freq: %d (0 is default)):\n", sda, scl, freq);
+
+#ifdef CORE_TEENSY
+      Wire.setSDA(sda);
+      Wire.setSCL(scl);
       Wire.begin();
+#else
+      Wire.begin(sda,scl);
+#endif
       nDevices = 0;
       for(address = 1; address < 127; address++ )
       {
